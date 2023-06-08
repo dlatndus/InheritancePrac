58889d004f902982d0c2d6db144faf0fddd1bd00
Sooyeon Lim <96305365+printyeon@users.noreply.github.com>
2023-06-08 14:07:31 +0900

    초기 커밋.

---
+++ InheritancePrac/Dog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InheritancePrac
+{
+    internal class Dog
+    {
+
+        public int Age { get; set; }
+        public int Color { get; set; }
+
+        public Dog() { 
+            this.Age = 0;
+        }
+        public void Eat()
+        {
+            Console.WriteLine("냠냠 먹습니다.");
+        }
+    }
+}
---
+++ InheritancePrac/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePrac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+        }
+    }
+}
